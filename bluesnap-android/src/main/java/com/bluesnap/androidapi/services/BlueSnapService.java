5d3b3f208b0436709f9b52b09ab5778775166ad3
Roy B <roy.biber@bluesnap.com>
2018-05-01 15:34:30 +0300

    Revert "moved state from full billing to independent"
    This reverts commit 92c4443

--- bluesnap-android/src/main/java/com/bluesnap/androidapi/services/BlueSnapService.java
+++ bluesnap-android/src/main/java/com/bluesnap/androidapi/services/BlueSnapService.java
@@ -246,10 +246,9 @@ public class BlueSnapService {
         if (null != billingInfo.getZip() && !"".equals(billingInfo.getZip()))
             postData.put(BillingInfo.BILLINGZIP, billingInfo.getZip());
 
-        if (BlueSnapValidator.checkCountryHasState(billingInfo.getCountry()))
-            postData.put(BillingInfo.BILLINGSTATE, billingInfo.getState());
-
         if (sdkRequest.isBillingRequired()) {
+            if (BlueSnapValidator.checkCountryHasState(billingInfo.getCountry()))
+                postData.put(BillingInfo.BILLINGSTATE, billingInfo.getState());
             postData.put(BillingInfo.BILLINGCITY, billingInfo.getCity());
             postData.put(BillingInfo.BILLINGADDRESS, billingInfo.getAddress());
         }
