9b42060ac1b3c0c0d13fa301a4e80c42f051cea3
roy.biber <roy.biber@bluesnap.com>
2018-06-13 17:27:10 +0300

    take merchant store currency in BlueSnap setup and not currency

--- DemoApp/src/main/java/com/bluesnap/android/demoapp/DemoMainActivity.java
+++ DemoApp/src/main/java/com/bluesnap/android/demoapp/DemoMainActivity.java
@@ -378,7 +378,6 @@ public class DemoMainActivity extends AppCompatActivity {
     }
 
     private void initControlsAfterToken() {
-
         final String merchantStoreCurrency = null != merchantStoreCurrencySpinner && null != merchantStoreCurrencySpinner.getSelectedItem() ? merchantStoreCurrencySpinner.getSelectedItem().toString() : "USD";
         //final String merchantStoreCurrency = (null == currency || null == currency.getCurrencyCode()) ? "USD" : currency.getCurrencyCode();
         bluesnapService.setup(merchantToken, tokenProvider, merchantStoreCurrency, getApplicationContext(), new BluesnapServiceCallback() {
