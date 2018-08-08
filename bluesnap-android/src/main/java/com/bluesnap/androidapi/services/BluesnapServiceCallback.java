4f5f8ce2c8256e609322f4548053bf0e71716688
snpori <oribsnap@gmail.com>
2018-08-08 19:12:15 +0300

    fixed some typos in member names

--- bluesnap-android/src/main/java/com/bluesnap/androidapi/services/BluesnapServiceCallback.java
+++ bluesnap-android/src/main/java/com/bluesnap/androidapi/services/BluesnapServiceCallback.java
@@ -4,9 +4,7 @@ package com.bluesnap.androidapi.services;
  * An interface to run async callbacks from services on the main thread.
  */
 public interface BluesnapServiceCallback {
-    //@MainThread
     void onSuccess();
 
-    //@MainThread
     void onFailure();
 }
