7252b45ca6dfcecf18927f4e3f80b58bb65b7fa5
roy.biber <roy.biber@bluesnap.com>
2018-08-13 10:13:35 +0300

    add comments and fail in catch

--- bluesnap-android/src/test/java/com/bluesnap/androidapi/SdkConfigurationTests.java
+++ bluesnap-android/src/test/java/com/bluesnap/androidapi/SdkConfigurationTests.java
@@ -75,16 +75,23 @@ public class SdkConfigurationTests extends TestCase {
     }
 
     @Test
-    public void create_shopper_json_test() {
+    public void create_json_shopper_for_update_shopper_server_test() {
         assertNotNull(sdkConfiguration);
         try {
             Shopper shopper = sdkConfiguration.getShopper();
+
+            // since test only populate previous used and we want to check the chosen card (New) than we populate new payment method
             shopper.setNewPaymentSources(shopper.getPreviousPaymentSources());
+
+            // add full credit card details
             shopper.getNewCreditCardInfo().getCreditCard().setCvc("123");
             shopper.getNewCreditCardInfo().getCreditCard().setNumber("4111 1111 1111 1111");
-            JSONObject shopperJsonObject = shopper.toJson();
 
+            // get shopper json
+            JSONObject shopperJsonObject = shopper.toJson();
             assertNotNull(shopperJsonObject);
+
+            // check contact info details in shopper
             assertEquals(shopperJsonObject.get("vaultedShopperId"), 22868797);
             assertEquals(shopperJsonObject.get("firstName"), "aaa2");
             assertEquals(shopperJsonObject.get("lastName"), "bbb2");
@@ -96,16 +103,20 @@ public class SdkConfigurationTests extends TestCase {
             assertEquals(shopperJsonObject.get("city"), "billing city2");
             assertEquals(shopperJsonObject.get("shopperCurrency"), "USD");
 
+            // get paymentSources from shopper
             JSONObject paymentSourcesJsonObject = (JSONObject) shopperJsonObject.get("paymentSources");
             assertNotNull(paymentSourcesJsonObject);
 
+            // get creditCardInfo Array from paymentSources
             JSONArray creditCardInfosJsonObject = (JSONArray) paymentSourcesJsonObject.get("creditCardInfo");
             assertNotNull(creditCardInfosJsonObject);
             assertEquals(creditCardInfosJsonObject.length(), 1);
 
+            // get first and only creditCardInfo from creditCardInfo Array
             JSONObject creditCardInfoJsonObject = creditCardInfosJsonObject.getJSONObject(0);
             assertNotNull(creditCardInfoJsonObject);
 
+            // get billingContactInfo from creditCardInfo
             JSONObject billingContactInfoJsonObject = (JSONObject) creditCardInfoJsonObject.get("billingContactInfo");
             assertNotNull(billingContactInfoJsonObject);
             assertEquals(billingContactInfoJsonObject.get("firstName"), "billingFirstName");
@@ -116,6 +127,7 @@ public class SdkConfigurationTests extends TestCase {
             assertEquals(billingContactInfoJsonObject.get("zip"), "01752");
             assertEquals(billingContactInfoJsonObject.get("country"), "us");
 
+            // get creditCard from creditCardInfo
             JSONObject creditCardJsonObject = (JSONObject) creditCardInfoJsonObject.get("creditCard");
             assertNotNull(creditCardJsonObject);
             assertEquals(creditCardJsonObject.get("cardLastFourDigits"), "1111");
@@ -124,9 +136,11 @@ public class SdkConfigurationTests extends TestCase {
             assertEquals(creditCardJsonObject.get("expirationMonth"), 12);
             assertEquals(creditCardJsonObject.get("expirationYear"), 2020);
 
+            // check added values from the beginning
             assertEquals(creditCardJsonObject.get("cardNumber"), "4111111111111111");
             assertEquals(creditCardJsonObject.get("securityCode"), "123");
 
+            // get shippingContactInfo from shopper
             JSONObject shippingContactInfoJsonObject = (JSONObject) shopperJsonObject.get("shippingContactInfo");
             assertNotNull(shippingContactInfoJsonObject);
             assertEquals(shippingContactInfoJsonObject.get("firstName"), "aaa2");
@@ -137,10 +151,14 @@ public class SdkConfigurationTests extends TestCase {
             assertEquals(shippingContactInfoJsonObject.get("zip"), "123452");
             assertEquals(shippingContactInfoJsonObject.get("city"), "shipping city2");
 
+            // get chosenPaymentMethod from shopper
             JSONObject chosenPaymentMethodJsonObject = (JSONObject) shopperJsonObject.get("chosenPaymentMethod");
             assertNotNull(chosenPaymentMethodJsonObject);
+
+            // check if  chosenPaymentMethodType is Credit Card
             assertEquals(chosenPaymentMethodJsonObject.get("chosenPaymentMethodType"), "CC");
 
+            // get creditCard from chosenPaymentMethodType
             JSONObject creditCard2JsonObject = (JSONObject) chosenPaymentMethodJsonObject.get("creditCard");
             assertNotNull(creditCard2JsonObject);
             assertEquals(creditCardJsonObject.get("cardLastFourDigits"), "1111");
@@ -150,7 +168,7 @@ public class SdkConfigurationTests extends TestCase {
             assertEquals(creditCardJsonObject.get("expirationYear"), 2020);
 
         } catch (JSONException e) {
-            e.printStackTrace();
+            fail("create json shopper for update shopper server fail:" + e.getMessage());
         }
 
     }
