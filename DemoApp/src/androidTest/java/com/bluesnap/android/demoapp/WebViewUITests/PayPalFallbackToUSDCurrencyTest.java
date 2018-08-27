8c21ebb8c2fd4f6d474d8f5256a597b7889b2efa
sivani <sivan.yakovich@bluesnap.com>
2018-08-27 14:40:42 +0300

    Rearranged PayPal UI tests

--- DemoApp/src/androidTest/java/com/bluesnap/android/demoapp/WebViewUITests/PayPalFallbackToUSDCurrencyTest.java
+++ DemoApp/src/androidTest/java/com/bluesnap/android/demoapp/WebViewUITests/PayPalFallbackToUSDCurrencyTest.java
@@ -1,22 +1,16 @@
 package com.bluesnap.android.demoapp.WebViewUITests;
 
-import com.bluesnap.android.demoapp.R;
 import com.bluesnap.androidapi.models.SdkRequest;
 import com.bluesnap.androidapi.services.BSPaymentRequestException;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static java.lang.Thread.sleep;
-
 /**
  * Created by sivani on 26/08/2018.
  */
 
-public class PayPalFallbackToUSDCurrencyTest extends PayPalTests {
+public class PayPalFallbackToUSDCurrencyTest extends PayPalWebViewTests {
     @Before
     public void setup() throws InterruptedException, BSPaymentRequestException {
         SdkRequest sdkRequest = new SdkRequest(purchaseAmount, "ILS"); //choose ILS as checkout currency
@@ -28,21 +22,6 @@ public class PayPalFallbackToUSDCurrencyTest extends PayPalTests {
 
     @Test
     public void pay_pal_transaction_fallback_to_usd_currency_test() throws InterruptedException {
-        onView(withId(R.id.payPalButton)).perform(click());
-
-        //wait for web to load
-        sleep(20000);
-
-        loginToPayPal();
-        submitPayPalPayment();
-
-        sdkResult = blueSnapService.getSdkResult();
-
-        //wait for transaction to finish
-        while ((payPalInvoiceId = sdkResult.getPaypalInvoiceId()) == null)
-            sleep(5000);
-
-        //verify transaction status
-        retrievePayPalTransaction();
+        payPalBasicTransaction();
     }
 }
--- DemoApp/src/androidTest/java/com/bluesnap/android/demoapp/WebViewUITests/PayPalWebViewTests.java
+++ DemoApp/src/androidTest/java/com/bluesnap/android/demoapp/WebViewUITests/PayPalWebViewTests.java
@@ -1,15 +1,10 @@
 package com.bluesnap.android.demoapp.WebViewUITests;
 
-import android.support.test.espresso.Espresso;
-import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.espresso.web.webdriver.DriverAtoms;
 import android.support.test.espresso.web.webdriver.Locator;
 import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
 
-import com.bluesnap.android.demoapp.CurrencyChangeTesterCommon;
-import com.bluesnap.android.demoapp.CustomFailureHandler;
 import com.bluesnap.android.demoapp.EspressoBasedTest;
 import com.bluesnap.android.demoapp.R;
 import com.bluesnap.androidapi.Constants;
@@ -17,7 +12,6 @@ import com.bluesnap.androidapi.http.BlueSnapHTTPResponse;
 import com.bluesnap.androidapi.http.HTTPOperationController;
 import com.bluesnap.androidapi.models.SdkRequest;
 import com.bluesnap.androidapi.services.BSPaymentRequestException;
-import com.bluesnap.androidapi.services.BlueSnapService;
 import com.bluesnap.androidapi.views.activities.WebViewActivity;
 
 import junit.framework.Assert;
@@ -26,12 +20,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.web.assertion.WebViewAssertions.webMatches;
 import static android.support.test.espresso.web.model.Atoms.getCurrentUrl;
@@ -49,8 +40,7 @@ import static org.hamcrest.CoreMatchers.containsString;
  * Created by sivani on 23/08/2018.
  */
 
-@RunWith(AndroidJUnit4.class)
-public class PayPalTests extends EspressoBasedTest {
+public class PayPalWebViewTests extends EspressoBasedTest {
 
     private final String SANDBOX_RETRIEVE_PAYPAL_TRANSACTION = "alt-transactions/";
     private final String SANDBOX_PAYPAL_EMAIL = "apiShopper@bluesnap.com";
@@ -74,79 +64,8 @@ public class PayPalTests extends EspressoBasedTest {
         setupAndLaunch(sdkRequest);
     }
 
-    @Test
-    public void pay_pal_back_button_test() throws InterruptedException {
-        onView(withId(R.id.payPalButton)).perform(click());
-
-        //wait for web to load
-        sleep(15000);
-
-        //verify that paypal url opened up
-        onWebView().check(webMatches(getCurrentUrl(), containsString(Constants.getPaypalSandUrl())));
-
-        Espresso.pressBack();
-
-        //verify we are back in choose payment methods
-        onView(withId(R.id.newCardButton))
-                .withFailureHandler(new CustomFailureHandler("pay_pal_back_button_test: New Card button is not displayed after pressing back"))
-                .check(matches(ViewMatchers.isDisplayed()));
-        onView(withId(R.id.payPalButton))
-                .withFailureHandler(new CustomFailureHandler("pay_pal_back_button_test: PayPal button is not displayed after pressing back"))
-                .check(matches(ViewMatchers.isDisplayed()));
-    }
-
-    @Test
-    public void pay_pal_entering_twice_test() throws InterruptedException {
-        onView(withId(R.id.payPalButton)).perform(click());
-
-        //wait for web to load
-        sleep(15000);
-
-        //verify that paypal url opened up
-        onWebView().check(webMatches(getCurrentUrl(), containsString(Constants.getPaypalSandUrl())));
-
-        Espresso.pressBack();
-
-        loginToPayPal();
-        submitPayPalPayment();
-    }
-
-    @Test
-    public void pay_pal_basic_transaction_test() throws InterruptedException {
-        onView(withId(R.id.payPalButton)).perform(click());
-
-        //wait for web to load
-        sleep(15000);
-
-        //verify that paypal url opened up
-        onWebView().check(webMatches(getCurrentUrl(), containsString(Constants.getPaypalSandUrl())));
-
-        loginToPayPal();
-        submitPayPalPayment();
-
-        sdkResult = BlueSnapService.getInstance().getSdkResult();
-
-        //wait for transaction to finish
-        while ((payPalInvoiceId = sdkResult.getPaypalInvoiceId()) == null)
-            sleep(5000);
-
-        //verify transaction status
-        retrievePayPalTransaction();
-    }
-
-    @Test
-    public void pay_pal_transaction_after_changing_currency_test() throws InterruptedException {
-        onView(withId(R.id.newCardButton)).perform(click());
-        CurrencyChangeTesterCommon.changeCurrency("GBP");
-        updateCurrencyAndAmount("USD", "GBP");
-
-        Espresso.pressBack();
-
-        onView(withId(R.id.payPalButton)).perform(click());
-
-        //wait for web to load
-        sleep(20000);
-
+    void payPalBasicTransaction() throws InterruptedException {
+        loadPayPalWebView();
         loginToPayPal();
         submitPayPalPayment();
 
@@ -160,6 +79,15 @@ public class PayPalTests extends EspressoBasedTest {
         retrievePayPalTransaction();
     }
 
+    void loadPayPalWebView() throws InterruptedException {
+        onView(withId(R.id.payPalButton)).perform(click());
+
+        //wait for web to load
+        sleep(20000);
+
+        //verify that paypal url opened up
+        onWebView().check(webMatches(getCurrentUrl(), containsString(Constants.getPaypalSandUrl())));
+    }
 
     void loginToPayPal() throws InterruptedException {
         try {
@@ -239,11 +167,7 @@ public class PayPalTests extends EspressoBasedTest {
     private void getTransactionStatus() {
         try {
             JSONObject jsonObject = new JSONObject(retrieveTransactionResponse);
-
             JSONObject jsonObjectProcessingInfo = jsonObject.getJSONObject("processingInfo");
-//            JSONObject jsonObjectProcessingStatus = jsonObject.getJSONObject("processingStatus");
-//            JSONArray creditCardInfoJsonArray = jsonObjectPaymentSources.getJSONArray("creditCardInfo");
-//            JSONObject jsonObjectBillingContactInfo = creditCardInfoJsonArray.getJSONObject(0).getJSONObject("billingContactInfo");
 
             String transactionStatus = getOptionalString(jsonObjectProcessingInfo, "processingStatus");
             Assert.assertEquals("PayPal transaction failed!", "SUCCESS", transactionStatus);
