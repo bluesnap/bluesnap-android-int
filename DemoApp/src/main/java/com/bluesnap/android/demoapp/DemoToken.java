be97919bb4d7d8efac80d1774a08bfaddda80df2
snpori <oribsnap@gmail.com>
2018-10-22 12:55:44 +0300

    fixing env fallback

--- DemoApp/src/main/java/com/bluesnap/android/demoapp/DemoToken.java
+++ DemoApp/src/main/java/com/bluesnap/android/demoapp/DemoToken.java
@@ -3,6 +3,9 @@ package com.bluesnap.android.demoapp;
 import com.bluesnap.androidapi.services.BluesnapToken;
 import com.bluesnap.androidapi.services.TokenProvider;
 
+import static com.bluesnap.android.demoapp.BuildConfig.BS_API_PASSWORD;
+import static com.bluesnap.android.demoapp.BuildConfig.BS_API_USER;
+
 /**
  * Created by roy.biber on 04/08/2016.
  * This file should follow SandboxToken from the sdk. but cannot replace it in order to keep the demo app build independent.
@@ -12,8 +15,8 @@ public class DemoToken extends BluesnapToken{
     public static final String SANDBOX_TOKEN_CREATION = "payment-fields-tokens";
     public static final String SANDBOX_CREATE_TRANSACTION = "transactions";
 
-    public static final String SANDBOX_USER = BuildConfig.BS_API_USER;
-    public static final String SANDBOX_PASS = BuildConfig.BS_API_PASSWORD;
+    public static final String SANDBOX_USER = BS_API_USER;
+    public static final String SANDBOX_PASS = BS_API_PASSWORD;
 
     public DemoToken(String merchantToken, TokenProvider tokenProvider) {
         super(merchantToken, tokenProvider);
