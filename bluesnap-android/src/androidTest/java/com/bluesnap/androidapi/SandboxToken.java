1b41923429ea381e785312ae8106d600e64f43ef
snpori <snpori@users.noreply.github.com>
2022-12-21 23:05:27 +0200

    fixing local.gradle inclusion

--- bluesnap-android/src/androidTest/java/com/bluesnap/androidapi/SandboxToken.java
+++ bluesnap-android/src/androidTest/java/com/bluesnap/androidapi/SandboxToken.java
@@ -3,6 +3,9 @@ package com.bluesnap.androidapi;
 import com.bluesnap.androidapi.services.BluesnapToken;
 import com.bluesnap.androidapi.services.TokenProvider;
 
+import static com.bluesnap.androidapi.BuildConfig.BS_API_PASSWORD;
+import static com.bluesnap.androidapi.BuildConfig.BS_API_USER;
+
 /**
  * Created by oz on 29/10/17.
  */
@@ -12,8 +15,8 @@ public class SandboxToken extends BluesnapToken {
     public static final String SANDBOX_TOKEN_CREATION = "payment-fields-tokens";
     public static final String SANDBOX_CREATE_TRANSACTION = "transactions";
 
-    public static final String SANDBOX_USER = BuildConfig.BS_API_USER;
-    public static final String SANDBOX_PASS = BuildConfig.BS_API_PASSWORD;
+    public static final String SANDBOX_USER = BS_API_USER;
+    public static final String SANDBOX_PASS = BS_API_PASSWORD;
 
     public SandboxToken(String merchantToken, TokenProvider tokenProvider) {
         super(merchantToken, tokenProvider);
